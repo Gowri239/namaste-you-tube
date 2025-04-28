a5292eed2a6c424b7cb13f128dde3dd9be101908
Gowri239 <gowrimopuru14@gmail.com>
2025-04-28 19:47:02 +0530

    add: search feature with debounce technique

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
+import { YOUTUBE_SEARCH_API } from "../utils/Constants";
 
 const Header = () => {
   const dispatch = useDispatch();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchSuggestions, setSearchSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
 
   const handleToggleMenu = () => {
     dispatch(toggleMenu());
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(getSearchSuggestions, 200);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const getSearchSuggestions = async () => {
+    const response = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const data = await response.json();
+
+    setSearchSuggestions(data[1]);
   };
 
   return (
@@ -22,14 +41,36 @@ const Header = () => {
         <div className="font-bold text-xl px-1 py-1">YouTube</div>
       </div>
       <div className="col-span-10 p-1">
-        <input
-          className="w-1/2 p-3 ml-30 border h-8 border-solid border-black rounded-l-full"
-          placeholder="Search"
-        />
-        <button className="cursor-pointer rounded-r-full border border-solid h-8 px-3 bg-gray-200 border-black">
-          🔍
-        </button>
+        <div className="relative w-1/2 ml-32">
+          <input
+            className="p-3 border h-8 border-solid border-black rounded-full w-full "
+            placeholder="Search"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
+          />
+          <button className="absolute right-0 top-0 h-8 px-3 rounded-r-full border border-solid bg-gray-200 border-black">
+            🔍
+          </button>
+
+          {showSuggestions && searchSuggestions.length > 0 && (
+            <div className="absolute bg-white py-2 my-2 shadow-xl rounded-lg w-full mt-1">
+              <ul>
+                {searchSuggestions.map((suggestion, index) => (
+                  <li
+                    key={index}
+                    className="px-4 py-2 hover:bg-gray-100 flex items-center"
+                  >
+                    🔍 {suggestion}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
+
       <div className="col-span-1 w-8">
         <img src="./user.png" alt="user" />
       </div>
--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,2 +1,8 @@
+const GOOGLE_API_KEY = "AIzaSyAqfdsvnMKSYpJnmQDlH7xFMQ-1G3h0BFA";
+
 export const YOUTUBE_VIDEOS_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=AIzaSyAqfdsvnMKSYpJnmQDlH7xFMQ-1G3h0BFA";
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+  GOOGLE_API_KEY;
+
+export const YOUTUBE_SEARCH_API =
+  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
